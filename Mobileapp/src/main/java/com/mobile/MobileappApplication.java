dd971ffdd75d893d374948c0e45f1596744142a2
Lavanyaramavath <lavanyaramavath343@gmail.com>
2023-03-26 11:29:37 +0530

    Mobbile app

---
+++ Mobileapp/src/main/java/com/mobile/MobileappApplication.java
@@ -0,0 +1,13 @@
+package com.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileappApplication.class, args);
+	}
+
+}
